5e30ef32747c721ee6a33d672d960f3a15b929ca
Ebrahim Emad <ebemad8@gmail.com>
2025-08-30 18:21:49 +0300

    adding producer

--- services/producer/app/producer.py
+++ services/producer/app/producer.py
@@ -3,7 +3,6 @@ import os
 import random
 import time
 from datetime import datetime
-
 from dateutil.tz import gettz
 from faker import Faker
 from kafka import KafkaProducer
@@ -11,15 +10,13 @@ KAFKA_BOOTSTRAP_SERVERS=os.getenv("KAFKA_BOOTSTRAP_SERVERS","kafka:9092")
 KAFKA_TOPIC=os.getenv("KAFKA_TOPIC", "transactions")
 RATE=float(os.getenv("PRODUCE_RATE_PER_SEC", "2"))
 
-fake = Faker()
+fake=Faker()
+
 
 def generate_transaction()->dict:
-    amount = round(random.lognormvariate(3, 1), 2)  # skewed positive amounts
-
-    # add some injected fraudulent patterns
+    amount=round(random.lognormvariate(3,1),2)
     if random.random()<0.05:
         amount*=random.uniform(5,20)
-
     return {
         "transaction_id": fake.uuid4(),
         "amount": float(amount),
@@ -28,10 +25,10 @@ def generate_transaction()->dict:
             "merchant_risk": float(random.random()),
             "hour": float(datetime.now(tz=gettz("Africa/Cairo")).hour),
         },
-        "event_time": datetime.utcnow().isoformat()+'Z',
+        "event_time": datetime.now(tz=gettz("Africa/Cairo")).isoformat(),
     }
 
-def main()->None:
+def main()-> None:
     producer=None
     while producer is None:
         try:
@@ -47,5 +44,5 @@ def main()->None:
         payload=generate_transaction()
         producer.send(KAFKA_TOPIC,value=payload)
         time.sleep(interval)
-if __name__=="__main__":
+if __name__=='__main__':
     main()
